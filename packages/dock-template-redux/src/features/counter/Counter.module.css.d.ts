75d45cf159a14528641ab4f704ed8423a18a3838
hidarikani <19387605+hidarikani@users.noreply.github.com>
2024-01-01 13:58:57 +0200

    Figured out CSS typings

---
+++ packages/dock-template-redux/src/features/counter/Counter.module.css.d.ts
@@ -0,0 +1,5 @@
+export const row: string;
+export const value: string;
+export const button: string;
+export const textbox: string;
+export const asyncButton: string;
--- packages/dock-template-redux/src/features/counter/Counter.tsx
+++ packages/dock-template-redux/src/features/counter/Counter.tsx
@@ -9,7 +9,7 @@ import {
   incrementIfOdd,
   selectCount,
 } from './counterSlice';
-import styles from './Counter.module.css';
+import * as styles from './Counter.module.css';
 
 export function Counter() {
   const count = useAppSelector(selectCount);
