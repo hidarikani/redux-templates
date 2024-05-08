940d6a8ea61992e0fbc2e414bc92260a15516ef9
hidarikani <19387605+hidarikani@users.noreply.github.com>
2024-05-08 14:53:45 +0300

    Configured Spectrum

--- packages/dock-template-redux/src/main.tsx
+++ packages/dock-template-redux/src/main.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import { defaultTheme, Provider as Spectrum } from '@adobe/react-spectrum';
+import { Provider as Redux } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Redux store={store}>
+      <Spectrum theme={defaultTheme}>
+        <App />
+      </Spectrum>
+    </Redux>
   </React.StrictMode>
 );
