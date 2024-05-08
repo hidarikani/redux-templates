1ddd5931002b3043e4356dd4d67404f0526a5f74
hidarikani <19387605+hidarikani@users.noreply.github.com>
2024-05-08 15:18:37 +0300

    Implemented responsive layout

--- packages/dock-template-redux/src/App.tsx
+++ packages/dock-template-redux/src/App.tsx
@@ -1,15 +1,73 @@
+import { Grid, Flex, View } from '@adobe/react-spectrum';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Grid
+      areas={{
+        base: ['header', 'nav', 'content', 'footer'],
+        M: [
+          'header   header',
+          'nav      content',
+          'nav      content',
+          'footer   footer',
+        ],
+        L: [
+          'header header  header',
+          'nav    content toc',
+          'nav    content toc',
+          'footer footer  footer',
+        ],
+      }}
+      columns={{
+        M: ['size-2000', '1fr'],
+        L: ['size-2000', '1fr', 'size-2000'],
+      }}
+      gap="size-100"
+    >
+      <View backgroundColor="gray-200" gridArea="header">
         <img src={logo} className="App-logo" alt="logo" />
+      </View>
+      <View backgroundColor="gray-200" gridArea="nav">
+        <Flex
+          direction={{ base: 'row', M: 'column' }}
+          gap="size-100"
+          margin="size-100"
+        >
+          <View
+            backgroundColor="static-gray-50"
+            height="size-250"
+            minWidth="size-900"
+          />
+          <View
+            backgroundColor="static-gray-50"
+            height="size-250"
+            minWidth="size-900"
+          />
+          <View
+            backgroundColor="static-gray-50"
+            height="size-250"
+            minWidth="size-900"
+          />
+        </Flex>
+      </View>
+      <View  gridArea="content" height="size-4600">
         <Counter />
-      </header>
-    </div>
+      </View>
+      <View
+        backgroundColor="gray-200"
+        gridArea="toc"
+        minHeight="size-1000"
+        isHidden={{ base: true, L: false }}
+      />
+      <View
+        backgroundColor="gray-200"
+        gridArea="footer"
+        height="size-1000"
+      />
+    </Grid>
   );
 }
 
